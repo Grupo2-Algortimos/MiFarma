7699513b89569c60d4b8af1b180eb9932ae0ba12
Cesar Augusto Arostegui Alzamora <u202114548@upc.edu.pe>
2024-09-20 17:16:08 -0500

    -

--- MiFarma/Source.cpp
+++ MiFarma/Source.cpp
@@ -1,16 +1,8 @@
-#include <iostream>
 #include "ControllerProgram.h"
-using namespace std;
-
-
-using namespace std;
-
 
 int main()
 {
     Programa* run = new Programa();
     run->menu();
-
-    
 }
 
