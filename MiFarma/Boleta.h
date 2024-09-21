9546c61dc71659f69746f62d20673d61e8ba9b1d
Dev-Shiro <148409876+Dev-Shiro@users.noreply.github.com>
2024-09-21 11:43:11 -0500

    Faltantes
    se implemento clase boleta, se implemento la lectura de archivos y la carga de archivos en las listas

---
+++ MiFarma/Boleta.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+template<class T>
+class Boleta
+{
+private:
+	T nombre;
+	T fecha;
+	T monto;
+public:
+	Boleta(T _nombre, T _fecha, T _monto) {
+		this->nombre = _nombre;
+		this->fecha = _fecha;
+		this->monto = _monto;
+	}
+	//metodos getter and setter
+	T getnombre() { return nombre; }
+	T getfecha() { return fecha; }
+	T getmonto() { return monto; }
+
+	void setnombre(T _nombre) { nombre = _nombre; }
+	void setfecha(T _fecha) { fecha = _fecha; }
+	void setmonto(T _monto) { monto = _monto; }
+
+
+};
+
